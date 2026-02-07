2633b212ff8c220c9af720e6049899964fe38efd
Shreyansh Madhav <madhavshreyansh@gmail.com>
2026-02-07 23:51:34 +0530

    small bug fixes

--- routes/restaurant.route.js
+++ routes/restaurant.route.js
@@ -3,7 +3,7 @@ import { verifyToken } from "../middleware/verify.js";
 
 
 export default function restaurantRoute(app) {
-    app.post('/api/restaurant', createRestaurant);
+    app.post('/api/restaurants', createRestaurant);
     app.get('/api/restaurants', verifyToken, fetchRestaurant);
     app.patch('/api/restaurant/:id', updateRestaurant);
     app.delete('/api/restaurant/:id', deleteRestaurant);
